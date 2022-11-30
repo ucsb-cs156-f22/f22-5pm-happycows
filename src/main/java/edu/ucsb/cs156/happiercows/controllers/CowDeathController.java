28d82894a7bb23c006fff060e711dbee945eb63c
Colter Sirlin <csirlin@ucsb.edu>
2022-11-29 17:36:02 -0800

    fixed spacing

--- src/main/java/edu/ucsb/cs156/happiercows/controllers/CowDeathController.java
+++ src/main/java/edu/ucsb/cs156/happiercows/controllers/CowDeathController.java
@@ -31,65 +31,65 @@ import edu.ucsb.cs156.happiercows.repositories.CowDeathRepository;
 @RequestMapping("/api/cowdeath")
 @RestController
 public class CowDeathController extends ApiController {
-	
-	@Autowired
-	private CowDeathRepository cowDeathRepository;
+    
+    @Autowired
+    private CowDeathRepository cowDeathRepository;
 
-	@Autowired
-	ObjectMapper mapper;
+    @Autowired
+    ObjectMapper mapper;
 
-	@ApiOperation(value = "Create a new CowDeath entity")
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	@PostMapping("")
-	public ResponseEntity<String> createCowDeath(
-		@ApiParam("commons_id") @RequestParam long commonsId,
-		@ApiParam("user_id") @RequestParam long userId,
-		@ApiParam("zonedDateTime") @RequestParam("zonedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime zonedDateTime,
-		@ApiParam("cowsKilled") @RequestParam Integer cowsKilled,
-		@ApiParam("avgHealth") @RequestParam double avgHealth) throws JsonProcessingException {
-		
-		CowDeath createdCowDeath = new CowDeath();
-		createdCowDeath.setCommonsId(commonsId);
-		createdCowDeath.setUserId(userId);
-		createdCowDeath.setZonedDateTime(zonedDateTime);
-		createdCowDeath.setCowsKilled(cowsKilled);
-		createdCowDeath.setAvgHealth(avgHealth);
-		
-		CowDeath savedCowDeath = cowDeathRepository.save(createdCowDeath);
-		String body = mapper.writeValueAsString(savedCowDeath);
-		return ResponseEntity.ok().body(body);
-	}
+    @ApiOperation(value = "Create a new CowDeath entity")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("")
+    public ResponseEntity<String> createCowDeath(
+        @ApiParam("commons_id") @RequestParam long commonsId,
+        @ApiParam("user_id") @RequestParam long userId,
+        @ApiParam("zonedDateTime") @RequestParam("zonedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime zonedDateTime,
+        @ApiParam("cowsKilled") @RequestParam Integer cowsKilled,
+        @ApiParam("avgHealth") @RequestParam double avgHealth) throws JsonProcessingException {
+        
+        CowDeath createdCowDeath = new CowDeath();
+        createdCowDeath.setCommonsId(commonsId);
+        createdCowDeath.setUserId(userId);
+        createdCowDeath.setZonedDateTime(zonedDateTime);
+        createdCowDeath.setCowsKilled(cowsKilled);
+        createdCowDeath.setAvgHealth(avgHealth);
+        
+        CowDeath savedCowDeath = cowDeathRepository.save(createdCowDeath);
+        String body = mapper.writeValueAsString(savedCowDeath);
+        return ResponseEntity.ok().body(body);
+    }
 
-	@ApiOperation(value = "List cow deaths for a given commons")
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
-	@GetMapping("/bycommons")
-	public ResponseEntity<String> listCommonsCowDeaths(
-		@ApiParam("commons_id") @RequestParam Long commonsId) throws JsonProcessingException {
-		
-		Iterable<CowDeath> cowDeathIter = cowDeathRepository.findAllByCommonsId(commonsId);
-		
-		ArrayList<CowDeath> cowDeathList = new ArrayList<CowDeath>();
-		cowDeathIter.forEach(cowDeathList::add);
-		
-		String body = mapper.writeValueAsString(cowDeathIter);
-		return ResponseEntity.ok().body(body);
+    @ApiOperation(value = "List cow deaths for a given commons")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/bycommons")
+    public ResponseEntity<String> listCommonsCowDeaths(
+        @ApiParam("commons_id") @RequestParam Long commonsId) throws JsonProcessingException {
+        
+        Iterable<CowDeath> cowDeathIter = cowDeathRepository.findAllByCommonsId(commonsId);
+        
+        ArrayList<CowDeath> cowDeathList = new ArrayList<CowDeath>();
+        cowDeathIter.forEach(cowDeathList::add);
+        
+        String body = mapper.writeValueAsString(cowDeathIter);
+        return ResponseEntity.ok().body(body);
 
-	}
+    }
 
-	@ApiOperation(value = "List cow deaths for a given commons and user")
-	@PreAuthorize("hasRole('ROLE_USER')")
-	@GetMapping("/byusercommons")
-	public ResponseEntity<String> listUserCommonsCowDeaths(
-		@ApiParam("commons_id") @RequestParam Long commonsId,
-		@ApiParam("user_id") @RequestParam Long userId) throws JsonProcessingException {
-		
-		Iterable<CowDeath> cowDeathIter = cowDeathRepository.findAllByCommonsIdAndUserId(commonsId, userId);
-		
-		ArrayList<CowDeath> cowDeathList = new ArrayList<CowDeath>();
-		cowDeathIter.forEach(cowDeathList::add);
-		
-		String body = mapper.writeValueAsString(cowDeathIter);
-		return ResponseEntity.ok().body(body);
+    @ApiOperation(value = "List cow deaths for a given commons and user")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/byusercommons")
+    public ResponseEntity<String> listUserCommonsCowDeaths(
+        @ApiParam("commons_id") @RequestParam Long commonsId,
+        @ApiParam("user_id") @RequestParam Long userId) throws JsonProcessingException {
+        
+        Iterable<CowDeath> cowDeathIter = cowDeathRepository.findAllByCommonsIdAndUserId(commonsId, userId);
+        
+        ArrayList<CowDeath> cowDeathList = new ArrayList<CowDeath>();
+        cowDeathIter.forEach(cowDeathList::add);
+        
+        String body = mapper.writeValueAsString(cowDeathIter);
+        return ResponseEntity.ok().body(body);
 
-	}
+    }
 }
